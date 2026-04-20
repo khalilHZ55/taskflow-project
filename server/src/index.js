6fbc98b4e9c3ca7eee56971fc1e6a7a8eb0dcbce
khalilHZ55 <khalilhanjri55@gmail.com>
2026-04-20 13:45:37 +0200

    eliminar barra final en origen CORS

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://taskflow-project-taupe.vercel.app/",  // ← dominio de tu frontend en Vercel
+    "https://taskflow-project-taupe.vercel.app",  // ← dominio de tu frontend en Vercel
   ]
 }));
 app.use(express.json());
